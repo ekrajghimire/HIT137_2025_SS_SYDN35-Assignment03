385152a4102a7ee6387a6a4e4fe2603b9fa1cd08
Ekraj Ghimire <manishg.official@gmail.com>
2026-01-22 10:23:00 +1100

    added child class for grayscale processing

--- editor.py
+++ editor.py
@@ -7,3 +7,10 @@ class ImageProcessor:
   def process(self, image):
     """ process method will be implmented by child classes for coressponding image processing logic"""
     raise NotImplementedError
+  
+
+# GrayscaleProcessor child class
+class GrayscaleProcessor(ImageProcessor):
+  def process(self,image):
+    """ implment the process logic for image grayscaling """
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
