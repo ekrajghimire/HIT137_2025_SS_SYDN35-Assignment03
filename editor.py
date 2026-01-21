9c9d7ee9fc9ae61d2839bc0b60b540c3fdd9a750
Roshan Lamichhane <lamichhanersn@gmail.com>
2026-01-21 14:45:00 +1100

    added parent class ImageProcessor

--- editor.py
+++ editor.py
@@ -1,2 +1,9 @@
 import cv2
 import tkinter as tk
+
+
+# Image Processor parent class
+class ImageProcessor:
+  def process(self, image):
+    """ process method will be implmented by child classes for coressponding image processing logic"""
+    raise NotImplementedError
