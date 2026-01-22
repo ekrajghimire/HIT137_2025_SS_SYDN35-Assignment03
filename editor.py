806d560e3a18b157491b463d196b1445f378f906
Ekraj Ghimire <manishg.official@gmail.com>
2026-01-22 11:03:00 +1100

    added child class for edge processing

--- editor.py
+++ editor.py
@@ -14,3 +14,12 @@ class GrayscaleProcessor(ImageProcessor):
   def process(self,image):
     """ implment the process logic for image grayscaling """
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# EdgeProcessor child class
+class EdgeProcessor(ImageProcessor):
+  def process(self, image):
+    """ implment the process logic for edge processing"""
+    # Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Canny works on grayscale images
+    return cv2.Canny(gray, 100, 200)
